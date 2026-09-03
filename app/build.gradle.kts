17b6fb5db43b38684adcd8efd80d9ba5b4976baf
Omar Alaaeldein <omar.alaaeldein@ejust.edu.eg>
2026-09-03 06:06:29 -0400

    chore: bump version to 1.1.2 for IPv4 live-token test APKs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.tailcat.vpn"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         if (!enableAbiSplits) {
